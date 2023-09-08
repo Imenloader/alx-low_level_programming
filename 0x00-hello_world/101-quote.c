dc41c8c07dff588416f31fd38afabf4f46fd9c91
Imenloader <greenfeeda@gmail.com>
2023-09-07 17:04:21 -0700

    first day with c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,8 @@
 */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	int quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19",
-	s);
-				return (1);
+	write(1, quo, 59);
+	return (1);
 }
